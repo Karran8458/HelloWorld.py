02f4e643afbca016e9e97caebb190ee2fb254cb0
Karran8458 <kgowda@eckerd.edu>
2020-05-11 15:52:44 -0400

    Create HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,27 @@
+print( "Hello World" );
+
+name = "Noelle";
+print(name);
+
+name = 42;
+print(name);
+
+fave_food1 = "sushi";
+fave_food2 = "pizza";
+print(fave_food1);
+print(fave_food2);
+
+name = "Karran";
+print(name);
+
+last_name = "Gowda";
+print(name+last_name);
+
+number = 25;
+print(name,number);
+
+print("I love to eat", fave_food1);
+
+>>> fave_food3 = "pizza"
+>>> "I love to eat, %s." % fave_food3
+
